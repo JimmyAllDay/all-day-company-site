473b0aa5ba60da3a5687d9d55e773afa02921c93
James Marshall <jameswhmarshallactions@gmail.com>
2023-07-19 19:20:40 +1000

    set up page structure and nav links

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div>
+      <div>{`© All Day Web Projects 2023. All Rights Reserved. :)`}</div>
+    </div>
+  );
+}
